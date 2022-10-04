8f49a5e0c1b954b66c31efaa938bddad2eefa98e
Tahar Arib <tab@mail.team.com>
2022-10-04 10:40:28 +0200

    first commit in Eclipse

---
+++ src/main/java/fr/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package fr.hello;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Maven World") ;
+
+	}
+
+}
