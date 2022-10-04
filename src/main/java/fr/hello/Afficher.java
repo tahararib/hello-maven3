b0ebdc429d1c4d940eebc563cc94390acd6badd9
Tahar Arib <tab@mail.team.com>
2022-10-04 11:10:16 +0200

    v3 améliorée

--- src/main/java/fr/hello/Afficher.java
+++ src/main/java/fr/hello/Afficher.java
@@ -6,6 +6,11 @@ public class Afficher {
 	 *	 * @param message
 	 *					display message
 	 */
+	/**
+	 * un comment 
+	 * @param message
+	 */
+	
  public void display(String message)
 	{
 	System.out.println(message) ;
